456fb0f94eaf1a924d1890b2667481a5b37551c0
meet987654 <160616727+meet987654@users.noreply.github.com>
2024-12-21 10:55:14 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      ['@babel/preset-react', {runtime: "automatic"}]
+    ],
+  };
+
+  //types of testing(developer)
+  //unit testing
+  //integration testing
+  //end to end testing
+
+  //setting up testing in our app
+  //install react testing library
+  //install jest
+  //install babel dependencies
+  //configure babel(babel.config.js)
+  //configure parcel configure file to disable default transplition(.parcelrc)
+  //jest congigration(npx jest --init)
+  //install jsdom library
+  //install @babel/preset-react -to make JSX work in react
+  //include  @babel/preset-react inside babel config
+  //install @testing-library/jest-dom
+   
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [  "./src/**/*.{html,js,ts,jsx,tsx}",
+              "./components/*.js",
+              "./foodapp.html",
+              "./utils/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
